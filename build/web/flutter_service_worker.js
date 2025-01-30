9352c0bc1a17b2e82610571e367985fc9cf18643
Ashutosh Kumar <ashu3699@gmail.com>
2025-01-30 22:53:11 +0530

    Update service worker version, modify resource hashes, and add .htaccess for URL rewriting

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "534e07e1dbb87ce5d4f7f6a3539123da",
+const RESOURCES = {"flutter_bootstrap.js": "2e191393d4636c5dffef84dca3f95727",
 "version.json": "0325640b78b9f97959290dc5ebb8b3ed",
 "index.html": "edd4e1167f0d486ce6327d2ae9a73d34",
 "/": "edd4e1167f0d486ce6327d2ae9a73d34",
